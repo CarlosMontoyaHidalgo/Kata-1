9ccd77a493890d30536eceebf58df68a989af2b1
carlo <carlo@10.194.241.228>
2019-10-02 08:55:10 +0100

    Primera versión de Kata 1

--- src/kata1/Main.java
+++ src/kata1/Main.java
@@ -3,6 +3,8 @@ package kata1;
 import java.util.Date;
 
 public class Main {
-    Person pau = new Person("Pau", new Date(84, 1, 10));
-    
+    public static void main(String [] args){
+        Person pau = new Person("Pau", new Date(84, 1, 10));
+        System.out.println(pau.getName() + " tiene " + pau.getAge() + " años.");
+    }
 }
